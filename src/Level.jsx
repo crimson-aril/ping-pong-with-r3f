aab8d2249ae1c4b93ac944eecd9c3e7d85d7ca28
Vengefulinsaan <rumman2454@gmail.com>
2026-02-17 14:06:22 +0600

    nn

--- src/Level.jsx
+++ src/Level.jsx
@@ -65,14 +65,14 @@ export function BlockStart({ position = [0, 0, 0] }) {
  * - Hamburger model as reward
  */
 export function BlockEnd({ position = [0, 0, 0] }) {
-  const hamburger = useGLTF('hamburger.glb') // Load 3D model
+  const hamburger = useGLTF(new URL('/hamburger.glb', import.meta.url).href) // Load 3D model
 
   // Make all meshes cast shadows
   hamburger.scene.children.forEach((mesh) => (mesh.castShadow = true))
 
   return (
     <group position={position}>
-      <Text font="/bebas-neue-v9-latin-regular.woff" scale={1} position={[0, 2.25, 2]}>
+      <Text font={new URL('/bebas-neue-v9-latin-regular.woff', import.meta.url).href} scale={1} position={[0, 2.25, 2]}>
         FINISH
         <meshBasicMaterial toneMapped={false} />
       </Text>
